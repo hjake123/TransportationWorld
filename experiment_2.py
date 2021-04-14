99387c8ea02c3226dc82cb244159d6041e9096ac
Hyperlynx <hjake123@gmail.com>
2021-04-14 16:25:52 -0500

    Revert unneeded changes

--- experiment_2.py
+++ experiment_2.py
@@ -16,7 +16,7 @@ style.use("ggplot")
 
 seed = int(input("Seed: "))
 random.seed(seed)
-run_num = 0
+
 def experiment_2(LEARNING_RATE, DISCOUNT, HM_STEPS):
   q_table = initialize_q_table()
   step = 0
@@ -55,10 +55,8 @@ def experiment_2(LEARNING_RATE, DISCOUNT, HM_STEPS):
           break
       
       # all drop off locations are filled
-      if (len(list(filter(lambda cell: cell.has_space() == False, drop_cells))) == len(drop_cells) or step == HM_STEPS) and run_num == 1:
+      if len(list(filter(lambda cell: cell.has_space() == False, drop_cells))) == len(drop_cells) or step == HM_STEPS:
         break
-      elif (len(list(filter(lambda cell: cell.has_space() == False, drop_cells))) == len(drop_cells) or step == HM_STEPS) and run_num != 1:
-        run_num = 1
     all_rewards.append(session_reward)
 
   plt.plot([i for i in range(len(all_rewards))], all_rewards)
